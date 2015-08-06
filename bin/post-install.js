5ca6541684f22c7784405d70bc35efb5facf6c3e
Tim Ambler <tkambler@gmail.com>
2015-08-06 08:51:56 -0500

    1.1.3

--- bin/post-install.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var fs = require('fs');
-var path = require('path');
-var src = path.resolve(__dirname, '..', 'app', 'templates', 'package.json.bak');
-var target = path.resolve(__dirname, '..', 'app', 'templates', 'package.json');
-fs.renameSync(src, target);
